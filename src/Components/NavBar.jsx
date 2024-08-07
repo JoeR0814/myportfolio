c6fb9d47f00bc9da5a806638b04ffccd5176e772
JoeR0814 <joereis0814@gmail.com>
2024-08-07 13:58:59 -0400

    I took the resume setion out of the out of the navbar

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -50,14 +50,6 @@ function NavBar() {
           </a>
           <a
             onClick={handleNav}
-            href='#resume'
-            className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
-          >
-            <FaFileAlt size={20} />
-            <span className='pl-4'>Resume</span>
-          </a>
-          <a
-            onClick={handleNav}
             href='#projects'
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-red-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200 '
           >
@@ -95,12 +87,6 @@ function NavBar() {
             className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
           >
             <GrProjects size={20} />
-          </a>
-          <a
-            href='#resume'
-            className='rounded-full shadow-lg bg-gray-100 shadow-white-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'
-          >
-            <FaFileAlt size={20} />
           </a>
           <a
             href='#projects'
