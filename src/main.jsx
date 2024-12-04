484adf8d1ef0ac59455011d2731875953f26c715
JoeR0814 <joereis0814@gmail.com>
2024-12-03 19:42:09 -0500

    add vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Analytics } from '@vercel/analytics/next';
+import './index.css';
+import App from './App.jsx';
+
+export default function RootLayout() {
+	return (
+		<html lang='en'>
+			<head>
+				<title>Next.js</title>
+			</head>
+			<body>
+				<Analytics />
+			</body>
+		</html>
+	);
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
+
