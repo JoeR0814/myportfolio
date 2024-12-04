4e99f87e21bd8400eb66a5b19b38dd564aea39cb
JoeR0814 <joereis0814@gmail.com>
2024-12-04 10:18:19 -0500

    i deleted vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/vite';
 import './index.css';
 import App from './App.jsx';
-
-export default function RootLayout() {
-	return (
-		<html lang='en'>
-			<head>
-				<title>Next.js</title>
-			</head>
-			<body>
-				<Analytics />
-			</body>
-		</html>
-	);
-}
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
