92b5def365128a2ba0f1688a0aa807f2de952e16
JoeR0814 <joereis0814@gmail.com>
2024-12-19 15:48:41 -0500

    fixed my descriptions on my work page

--- src/Components/PortWork.jsx
+++ src/Components/PortWork.jsx
@@ -2,32 +2,32 @@ import PortWorkItem from './PortWorkItem';
 
 const data = [
 	{
-		year: 2010,
-		title: 'Construction',
-		duration: '5 years',
+		year: 2023 - 2024,
+		title: 'Full Stack Software Web Development',
+		duration: '2 years (2022-2024)',
 		details:
-			'Worked on new and old commercial/residential construction projects, highway road paving, and bridge construction work on old and new bridges.',
-	},
-	{
-		year: 2016,
-		title: 'Movie/TV Studio Industry',
-		duration: '4 years (2015-2019)',
-		details:
-			'Worked on TV shows such as American Horror Story (seasons 7 & 9), 9-1-1 (seasons 1 & 2), and This Is Us. I was the location manager for 9-1-1, responsible for logistics at each location to build sets and scenes. Worked at Fox Studios, Paramount Studio, Warner Brothers, and Disney Studio among others.',
+			'In October 2023, I made the pivotal decision to change careers and pursue my passion for software development. I enrolled in Goodwin University’s Full Stack Web Development program, where I gained comprehensive skills in front-end and back-end development. After completing the program and becoming a certified software developer in just six months, I have been dedicating my time to building my portfolio and working on personal projects to further develop my expertise and prepare for this exciting career transition.',
 	},
 	{
 		year: 2020,
 		title: 'Reis Transportation',
 		duration: '2 years (2020-2022)',
 		details:
-			'Started my own hauling business, transporting general goods with my own truck and 30-foot gooseneck trailer up and down the east coast.',
+			' I started my own hauling business. Using my truck and a 30-foot gooseneck trailer, I transported general goods up and down the East Coast. This experience allowed me to develop strong organizational skills, time management, and the ability to navigate complex logistics while running a successful operation.',
 	},
 	{
-		year: 2023,
-		title: 'Handyman and Education for Full Stack Software Web Development',
-		duration: '2 years (2022-2024)',
+		year: 2016,
+		title: 'Movie/TV Studio Industry',
+		duration: '4 years (2015-2019)',
 		details:
-			'In 2019, I got married and now have a son. Wanting to stay close to home, I chose to stop hauling and took up handy work while deciding my next steps. By the end of 2023, I decided to pursue a career in web development. In January 2024, I enrolled in Goodwin College in Hartford,Connecticut, to become a Full Stack Web Developer. I attended a bootcamp by UpRight Education, where I learned JavaScript and the MERN stack. I graduated in July 2024, having gained extensive knowledge and skills. I am excited to continue learning and working in the field of web development.',
+			'I worked on major TV shows, including American Horror Story (Seasons 7 & 9), 9-1-1 (Seasons 1 & 2), and This Is Us. As the location manager for 9-1-1, I was responsible for coordinating logistics at filming locations, ensuring seamless set and scene construction. My experience also includes working at renowned studios such as Fox Studios, Paramount Studios, Warner Brothers, and Disney Studios, where I honed my skills in organization, communication, and problem-solving within high-pressure environments.',
+	},
+	{
+		year: 2010,
+		title: 'Construction',
+		duration: '5 years',
+		details:
+			'"In 2010, I began my career in construction, gaining hands-on experience in both commercial and residential projects. My work included new builds, renovations, highway road paving, and bridge construction on both new and existing structures. This role allowed me to develop a strong foundation in project management, teamwork, and problem-solving while contributing to diverse and challenging construction projects.',
 	},
 ];
 
